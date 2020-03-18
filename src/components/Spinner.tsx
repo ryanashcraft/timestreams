1bcffde93df6deacb07df3ca1b899514bdebaf80
Ryan Ashcraft <ryan@ryanashcraft.me>
2020-03-17 17:37:00 -0700

    Improve spinner design

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
 
+import './Spinner.css'
+
 export const Spinner = () => {
-    return <div>…</div>
+    const [counter, setCounter] = React.useState(0)
+
+    React.useEffect(() => {
+        let intervalId = setInterval(() => {
+            setCounter(counter => counter + 1)
+        }, 200)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
+
+    return (
+        <div className="Spinner">
+            {Array.from({ length: 3 }, (_, i) => i).map(i => {
+                return (
+                    <div
+                        key={i}
+                        className="Spinner-dot"
+                        style={{ opacity: counter % 3 == i ? 1 : 0.5 }}
+                    ></div>
+                )
+            })}
+        </div>
+    )
 }
